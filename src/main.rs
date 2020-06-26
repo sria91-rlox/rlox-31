c86527a1513c09d52e964afdea801413bd463fd1
ethanae <eemandien@mweb.co.za>
2020-06-26 21:42:06 +0200

    - add run file function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
     match args.len() {
         1 => println!("Interpreting..."),
-        2 => println!("Running file {:?}", &args[1]),
+        2 => run_file(&args[1]),
         _ => println!("Usage: rlox [script]")
     }
 }
 
-
+fn run_file(file_name: &String) {
+    println!("=================\nRunning file {}\n=================", file_name);
+    let contents = fs::read_to_string(file_name)
+        .expect("Error reading the file");
+    println!("Contents: {}", contents);
+}
