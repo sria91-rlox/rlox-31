06d68ea3173bc4be27a71e480dc53084f188843d
ethanae <eemandien@mweb.co.za>
2020-06-26 22:36:49 +0200

    - add run_repl fn

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::env;
 use std::error::Error;
 use std::fs;
+use std::io;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args.len() {
-        1 => println!("Interpreting..."),
+        1 => run_repl(),
         2 => {
             if let Err(e) = run_file(&args[1]) {
                 println!("Error running file: {}", e);
@@ -22,4 +23,17 @@ fn run_file(file_name: &String) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(file_name)?;
     println!("Contents: {}", contents);
     Ok(())
+}
+
+fn run_repl() {
+    let stdin = io::stdin();
+    let input = &mut String::new();
+    loop {
+        print!("> ");
+        input.clear();
+        match stdin.read_line(input) {
+            Ok(_) => println!("{}", input),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
 }
