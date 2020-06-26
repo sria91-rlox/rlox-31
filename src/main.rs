4d0de863f19023417e14ecbd54a7dc812a2c3785
ethanae <eemandien@mweb.co.za>
2020-06-26 21:31:50 +0200

    - add initial cli entry

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
     
+    match args.len() {
+        1 => println!("Interpreting..."),
+        2 => println!("Running file {:?}", &args[1]),
+        _ => println!("Usage: rlox [script]")
+    }
 }
 
 
