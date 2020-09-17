433b698e7e9c102c195075b59a7f439a8e713bc4
ethanae <eemandien@mweb.co.za>
2020-09-17 23:08:57 +0200

    - add initial token scanning

---
+++ src/scanner.rs
@@ -0,0 +1,83 @@
+
+use crate::error_reporter::error;
+use crate::token::Token;
+use crate::token_type::TokenType;
+
+pub struct Scanner {
+  source: &'static str,
+  tokens: Vec<Token>,
+  start: u32,
+  current: u32,
+  line: u32
+}
+
+trait Lexer {
+  fn scan_tokens(&self) -> Vec<Token>;
+  fn is_at_end(&self) -> bool;
+  fn scan_token(&self) -> Token;
+  fn get_next_char(&self) -> char;
+  fn add_token(&self, token: TokenType);
+  fn add_token_literal(&self, token: TokenType, literal: &str);
+}
+
+impl Lexer for Scanner {
+  fn scan_tokens(&self) -> Vec<Token> {
+    let mut tokens = vec![]; 
+    while !self.is_at_end() {
+
+      // tokens.push(value: T)
+    } 
+    tokens.push(Token {
+      token: TokenType::Eof,
+      lexeme: "some part of speech",
+      line: 1,
+      literal: "super literal"
+    });
+    return tokens;
+  }
+
+  fn scan_token(&self) -> Token {
+    let character = self.get_next_char();
+    match character {
+      '(' => self.add_token(TokenType::LeftParen),
+      ')' => self.add_token(TokenType::RightParen),
+      '{' => self.add_token(TokenType::LeftBrace),
+      '}' => self.add_token(TokenType::RightBrace),
+      ',' => self.add_token(TokenType::Comma),
+      '.' => self.add_token(TokenType::Dot),
+      '-' => self.add_token(TokenType::Minus),
+      '+' => self.add_token(TokenType::Plus),
+      ';' => self.add_token(TokenType::SemiColon),
+      '*' => self.add_token(TokenType::Star),
+      _ => error(self.line, "Unexpected character")
+    }
+  }
+  
+  fn add_token(&self, token: TokenType) {
+    self.add_token_literal(token, "");
+  }
+
+  fn add_token_literal(&self, token: TokenType, literal: &str) {
+    let lexeme: &String = &self.source
+      .chars()
+      .skip(self.start as usize)
+      .take(self.current as usize)
+      .collect();
+    self.tokens.push(Token {
+      token,
+      lexeme,
+      line: 1,
+      literal: "super literal"
+    });
+  }
+
+  fn get_next_char(&self) -> char {
+    self.current += 1;
+    let next = self.source.as_bytes()[self.current as usize];
+    return next as char;
+  }
+
+  fn is_at_end(&self) -> bool {
+    return self.current >= self.source.len() as u32;
+  }
+}
