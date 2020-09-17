e98a4a951d56d4a8f1c8ba390fe8a0ef5c47c443
ethanae <eemandien@mweb.co.za>
2020-09-17 23:08:27 +0200

    - organise tokentype enum

--- src/token_type.rs
+++ src/token_type.rs
@@ -1,20 +1,49 @@
 pub enum TokenType {
   // Single-character tokens.
-  LeftParen, RightParen, LeftBrace, RightBrace,
-  Comma, Dot, Minus, Plus, SemiColon, Slash, Star,
+  LeftParen,
+  RightParen,
+  LeftBrace,
+  RightBrace,
+  Comma,
+  Dot,
+  Minus,
+  Plus,
+  SemiColon,
+  Slash,
+  Star,
 
   // One or two character tokens.
-  Bang, BangEqual,
-  Equal, EqualEqual,
-  Greater, GreaterEqual,
-  Less, LessEqual,
+  Bang,
+  BangEqual,
+  Equal,
+  EqualEqual,
+  Greater,
+  GreaterEqual,
+  Less,
+  LessEqual,
 
   // Literals.
-  Identifier, STRING, Number,
+  Identifier,
+  STRING,
+  Number,
 
   // Keywords.
-  And, Class, Else, False, Fun, For, If, Nil, Or,
-  Print, Return, Super, This, True, Var, While,
+  And,
+  Class,
+  Else,
+  False,
+  Fun,
+  For,
+  If,
+  Nil,
+  Or,
+  Print,
+  Return,
+  Super,
+  This,
+  True,
+  Var,
+  While,
 
   Eof
 }
